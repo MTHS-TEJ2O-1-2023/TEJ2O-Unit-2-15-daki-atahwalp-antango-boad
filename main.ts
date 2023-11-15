3ec0c67b9337f239a8cc3b4781a27cf2501865f6
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-11-15 12:55:43 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,4 +5,27 @@
  * This program lights up around the Microbit edge
 */
 
-basic.showString('Hello, World!')
+// variables
+let ledSprite: number = 0
+let sprite: game.LedSprite = null
+
+// on start
+basic.showIcon(IconNames.Happy)
+
+// on button A, starting loop to make LED light up around the Microbits edge
+input.onButtonPressed(Button.A, function () {
+  // setting values
+  basic.clearScreen()
+  ledSprite = 0
+  sprite = game.createSprite(0, 0)
+
+  // Boolean expression
+  while (ledSprite <= 17){
+    sprite.set(LedSpriteProperty.X, 0)
+    sprite.set(LedSpriteProperty.Y, 0)
+    sprite.move(1)
+    sprite.turn(Direction.Right, 90)
+    ledSprite++
+  }
+
+})
